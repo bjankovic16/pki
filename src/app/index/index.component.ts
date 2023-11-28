5e123a3a8b42917c8eff1970ab39f6a4fd8086cb
bjankovic16 <bjankovic16@gmail.com>
2023-11-28 16:00:58 +0100

    dodati su i proizvodi 90% kraj

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -9,11 +9,12 @@ import { Slatkis } from '../models/slatkis';
 })
 export class IndexComponent {
   ngOnInit() {
-    localStorage.clear();
+    //localStorage.clear();
     if (JSON.parse(localStorage.getItem("inicijalizacija")) == null) {
       localStorage.setItem("inicijalizacija", JSON.stringify(true));
       this.inicKorisnici();
       this.inicSlatkisi();
+      console.log("ovde");
     }
   }
 
@@ -56,26 +57,102 @@ export class IndexComponent {
     palacinkeVocne.komentari.push("Cena pristupačna. Odličan ukus. Preporučujem.");
     palacinkeVocne.komentari.push("Mnoooogo slatko");
 
-
     let palacinkeKing = new Slatkis();
-    palacinkeKing.opis = "Palačinke sa voćnim prelivom savršeni izbor za sve ljubitelje palačinki."
-    palacinkeKing.sastojci = "Jaja, brašno, med, mleko, jagode, borovnice, voćni preliv."
-    palacinkeKing.cena = 300;
+    palacinkeKing.naziv = "King"
+    palacinkeKing.opis = "King palačinka samo za prave kraljeve."
+    palacinkeKing.sastojci = "Jaja, brašno, med, mleko, jagode, šlag, borovnice, čokoladni preliv preliv."
+    palacinkeKing.cena = 500;
     palacinkeKing.slika = "../../assets/kolaci/king.png"
 
     let palacinkeMedodjija = new Slatkis();
-    palacinkeMedodjija.opis = "Palačinke sa voćnim prelivom savršeni izbor za sve ljubitelje palačinki."
-    palacinkeMedodjija.sastojci = "Jaja, brašno, med, mleko, jagode, borovnice, voćni preliv."
-    palacinkeMedodjija.cena = 300;
+    palacinkeMedodjija.naziv = "Medođija"
+    palacinkeMedodjija.opis = "Palačinke medođija kao da su je pčele pravile."
+    palacinkeMedodjija.sastojci = "Jaja, brašno, med, mleko, voćni preliv."
+    palacinkeMedodjija.cena = 600;
     palacinkeMedodjija.slika = "../../assets/kolaci/medodjija.png"
+
+    let bombonice = new Slatkis();
+    bombonice.naziv = "Čoko bomba";
+    bombonice.opis = "Samo za prave ljubitelje čokolade.";
+    bombonice.sastojci = "Jaja, brašno, čokolada, malina, šećer, med.";
+    bombonice.cena = 750;
+    bombonice.slika = "../../assets/kolaci/bombonice_kolac.png";
+
+    let cokoladni = new Slatkis();
+    cokoladni.naziv = "Čokoooos";
+    cokoladni.opis = "Samo za prave ljubitelje istančanog ukusa.";
+    cokoladni.sastojci = "Jaja, brašno, čokolada, griz, šećer.";
+    cokoladni.cena = 650;
+    cokoladni.slika = "../../assets/kolaci/cokoladni_kolac.png";
+
+    let vanilice = new Slatkis();
+    vanilice.naziv = "Vanilice";
+    vanilice.opis = "Samo za ljubitelje dobrih i proverenih stvari.";
+    vanilice.sastojci = "Jaja, brašno, vanila, griz, šećer, jaja.";
+    vanilice.cena = 450;
+    vanilice.slika = "../../assets/kolaci/vanilice_kolac.png";
 
     let kolaci = [];
     kolaci.push(palacinkeVocne);
     kolaci.push(palacinkeKing);
     kolaci.push(palacinkeMedodjija);
-    kolaci.push(palacinkeKing);
+    kolaci.push(bombonice);
+    kolaci.push(cokoladni);
+    kolaci.push(vanilice);
+
+
+    let rozalija = new Slatkis();
+    rozalija.naziv = "Rozalija";
+    rozalija.opis = "Samo za ljubitelje novih ukusa.";
+    rozalija.sastojci = "Jaja, brašno, vanila, griz, šećer, jaja, čokolada, šlag, piškote.";
+    rozalija.cena = 3000;
+    rozalija.slika = "../../assets/torte/rozalija_torta.png";
+
+    let cokolino = new Slatkis();
+    cokolino.naziv = "Čokolino";
+    cokolino.opis = "Ovo morate probati, najnoviji ukus.";
+    cokolino.sastojci = "Jaja, brašno, vanila, griz, šećer, jaja, čokolada, šlag, piškote, malina, nana, plazma.";
+    cokolino.cena = 5000;
+    cokolino.slika = "../../assets/torte/cokolino_torta.png";
+
+    let borovica = new Slatkis();
+    borovica.naziv = "Borovica";
+    borovica.opis = "Ukus borovnice sa kupinama na vrhu sigurno neće razočarati.";
+    borovica.sastojci = "Jaja, brašno, vanila, griz, šećer, jaja, čokolada, šlag, piškote, kupine, borovnice, plazma.";
+    borovica.cena = 4000;
+    borovica.slika = "../../assets/torte/borovica_torta.png";
+    
+    let vocna = new Slatkis();
+    vocna.naziv = "Voćni specijal";
+    vocna.opis = "Pravo osveženje u vrelim danima.";
+    vocna.sastojci = "Jaja, brašno, vanila šećer, jaja, čokolada, šlag, piškote, kupine, borovnice, jagode, maline, plazma.";
+    vocna.cena = 4900;
+    vocna.slika = "../../assets/torte/vocni_torta.png";
+
+    let ciz = new Slatkis();
+    ciz.naziv = "Čiz kejk";
+    ciz.opis = "Nešto novooooo.";
+    ciz.sastojci = "Jaja, brašno, vanila šećer, jaja, čokolada, šlag, piškote, plazma.";
+    ciz.cena = 7000;
+    ciz.slika = "../../assets/torte/ciz_torta.png";
+
+    let rodj = new Slatkis();
+    rodj.naziv = "Rođendanska";
+    rodj.opis = "Najbolje za rođendan.";
+    rodj.sastojci = "Jaja, brašno, vanila šećer, jaja, čokolada, šlag, piškote, plazma.";
+    rodj.cena = 10000;
+    rodj.slika = "../../assets/torte/rodjendanska_torta.png";
+
+    let torte = [];
+    torte.push(rozalija);
+    torte.push(cokolino);
+    torte.push(borovica);
+    torte.push(vocna);
+    torte.push(ciz);
+    torte.push(rodj);
+
     localStorage.setItem("kolac", JSON.stringify(kolaci));
-    localStorage.setItem("torta", JSON.stringify([]))
+    localStorage.setItem("torta", JSON.stringify(torte))
   }
 
 }
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TimerService } from '../services/timer';
-import { Slatkis } from '../models/slatkis';
 import { Router } from '@angular/router';
 
 @Component({
@@ -14,10 +13,10 @@ export class MainPageComponent implements OnInit, OnDestroy {
   constructor(private timerService: TimerService,private router:Router) {
   }
   
-  image1:string="../../assets/promocija1.png";
-  image2:string="../../assets/promocija2.png";
-  image3:string="../../assets/promocija3.png";
-  image4:string="../../assets/promocija4.png";
+  image1:string="../../assets/promocije/promocija1.png";
+  image2:string="../../assets/promocije/promocija2.png";
+  image3:string="../../assets/promocije/promocija3.png";
+  image4:string="../../assets/promocije/promocija4.png";
 
   ngOnInit(): void {
     this.timerService.startTimer(() => {
